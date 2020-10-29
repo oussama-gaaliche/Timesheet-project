10af4b0748da7d707e4c4fd0581aa2d551cb4e37
manel mnif <manel.mnif@esprit.tn>
2020-10-29 23:07:27 +0100

    junit

---
+++ src/test/java/tn/esprit/spring/DepartementServiceImplTest.java
@@ -0,0 +1,32 @@
+package tn.esprit.spring;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Departement;
+
+import tn.esprit.spring.services.IDepartementService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepartementServiceImplTest {
+	
+	@Autowired
+	IDepartementService DS;
+	
+	@Test
+	 public void testgetallDepartement() throws ParseException {
+		    List<Departement> listdepartement=DS.getAllDepartements();
+			assertEquals(2,listdepartement.size());
+		
+		}
+
+}
