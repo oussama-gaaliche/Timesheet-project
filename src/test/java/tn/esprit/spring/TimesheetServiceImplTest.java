fc6c810c6edb63569ae0899399a6fa3ba367e341
majdilahsini <majdi.lahsini@esprit.tn>
2020-11-02 13:41:44 +0100

    Add Timesheet unit test

--- src/test/java/tn/esprit/spring/TimesheetServiceImplTest.java
+++ src/test/java/tn/esprit/spring/TimesheetServiceImplTest.java
@@ -2,6 +2,8 @@ package tn.esprit.spring;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.entities.Timesheet;
+import tn.esprit.spring.entities.TimesheetPK;
 import tn.esprit.spring.services.ITimesheetService;
 
 @RunWith(SpringRunner.class)
@@ -22,6 +26,22 @@ public class TimesheetServiceImplTest {
 	public void testajouterMission(){
 		Mission mission = new Mission("Encadrer", "Encadrer les PFEs");
 		assertEquals(mission.getName(), ts.ajouterMission(mission).getName());
+	}
+	
+	@Test
+	public void testajouterTimesheet(){
+		
+		TimesheetPK timesheetPK = new TimesheetPK();
+		timesheetPK.setDateDebut(new Date());
+		timesheetPK.setDateFin(new Date());
+		timesheetPK.setIdEmploye(1);
+		timesheetPK.setIdMission(1);
+		
+		Timesheet timesheet = new Timesheet();
+		timesheet.setTimesheetPK(timesheetPK);
+		timesheet.setValide(false); //par defaut non valide
+		
+		assertEquals(timesheet.getMission(), ts.ajouterTimesheet(1, 1, new Date(), new Date()).getMission());
 	}
 		
 	
